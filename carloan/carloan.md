990229de18dce1c8e751db832e6f85b1567b7ac6
Khaled Jaafar <khaled@jaafar.ca>
2024-04-09 01:53:05 +0000

    fixed buggy program

---
+++ carloan/carloan.md
@@ -0,0 +1,35 @@
+#LOAN AMOUNT (P)
+This is the principal loan amount. It is the amount of money that the borrower wishes to borrow.
+
+#Annual Percentage Rate (APR)
+This is the rate that the loan is advertised as on an annual basis. Which differs from the actual interest to be paid on a monthly basis. 
+
+#Loan Duration (D)
+The number of months in which the entire principal loan amount is to be paid.
+
+#Monthly Interest Rate (MIR)
+This figure is achieved by dividing the APR by the number of months in the year.
+
+MIR = APR / 12
+
+#Monthly Interest Accrued (MIA)
+Multiplying the monthly interest rate by the principal amount minus payments made (Remaining Loan Amount), will yield us the dollar amount of interest accrued on the loan during the most recent period.
+
+MIA = RLA * MIR
+
+#Monthly Payment on Principal (MPP)
+We will divide the principal amount by the loan duration in months to determine the amount of principal to pay in each payment. This means that each month, an equal payment is made against the principal.
+
+MPP = P / D
+
+#Monthly Payment (MP)
+The monthly payment is reached by adding the monthly principal payment amount, and the monthly interest accrued during the period to which the payment occurs.
+
+MP = MPP + MIA
+
+#PYLINT Errors
+The pylint scan of my file returned a very low score, due to code E0602, which I do not quite understand as the program works. 
+
+
+
+
--- carloan/carloan.py
+++ carloan/carloan.py
@@ -2,24 +2,45 @@
 def stringdc(string):
     return round(float(string), 2)
 
-def dc(float):
-    return round(float, 2)
+def dc(number):
+    return round(number, 2)
 
 print("Welcome to the car loan calculator!")
 #GET principal, apr, duration
+def get_principal():
+    principal = 0
+    while principal <= 0:
+        principal = stringdc(input("Please input the total loan amount: "))
+    print("principal = ", principal)
+    return principal
 
-PRINCIPAL = stringdc(input("Please input the total loan amount: "))
-APR = stringdc(input("Please input the APR: "))
-DURATION = stringdc(input("Please input the loan duration in months: "))
+def get_apr():
+    apr = -1
+    while apr < 0:
+        apr = stringdc(input("Please input the apr: "))
+    print("apr = ", apr)
+    return apr
 
-#CALCULATE monthly interest rate 
-MIR = dc((APR / 12)/100)
-print("MIR = ", MIR * 100, "%")
-MPP = dc(PRINCIPAL / DURATION)
-print("MPP = ", MPP)
-mia = dc(PRINCIPAL * MIR)
-print("mia = ", mia)
+def get_duration():
+    duration = 0
+    while duration <= 0:
+        duration = stringdc(
+            input("Please input the loan duration in months: ")
+        )
+    print("duration = ", duration)
+    return duration
 
-#CALCULATE the first monthly payment using 
-mp = MPP + mia
-print(f"Your next monthly payment amount is {mp}")
+principal = get_principal()
+apr = get_apr()
+duration = get_duration()
+#CALCULATE monthly interest rate
+mir = round((apr / 12)/100, 4)
+print("mir = ", mir * 100, "%")
+mpp = dc(principal / duration)
+print("mpp = ", mpp)
+mia = dc(principal * mir)
+print("mia = ", dc(mia))
+
+#CALCULATE the first monthly payment using
+mp = dc(mpp + mia)
+print(f"Your next monthly payment amount is ${mp}")
